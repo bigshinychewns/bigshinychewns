38e423e59a26e427fb2deaa5280701a0d16da250
bigshinychewns <bigshinychewns@gmail.com>
2023-05-23 20:19:49 -0600

    Switch to Render.com config

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,30 @@
 // import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-// import node from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-vercel';
 
+// --=== VERCEL CONFIG ===--
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
 // const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
+
 // 	kit: {
 // 		adapter: adapter()
 // 	}
 // };
 
 // export default config;
+
+// --=== RENDER CONFIG ===--
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
