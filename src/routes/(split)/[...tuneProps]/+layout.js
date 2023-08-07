531bedcd5405753a9d35a582560bb7b8563daeba
bigshinychewns <bigshinychewns@gmail.com>
2023-08-07 13:16:19 -0600

    Add logging to tuneProps

--- src/routes/(split)/[...tuneProps]/+layout.js
+++ src/routes/(split)/[...tuneProps]/+layout.js
@@ -1,6 +1,8 @@
 /** @type {import('./$types').LayoutLoad} */
 export function load({ params }) {
+	console.log('wtf is tuneprops??', params.tuneProps);
 	const [tuneName, tuneVersion] = params.tuneProps.split('/');
+	console.log('[tuneName, tuneVersion]', [tuneName, tuneVersion]);
 	return {
 		tuneName,
 		tuneVersion
